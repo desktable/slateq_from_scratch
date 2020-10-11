2133f02b517091b284581351c5859f16b80896af
Henry Chen <chendaizhuo@gmail.com>
2020-10-10 23:32:43 -0700

    Implemented Q model

--- slateq_from_scratch/main.py
+++ slateq_from_scratch/main.py
@@ -5,7 +5,9 @@ import numpy as np
 import torch
 import torch.nn as nn
 from ray.rllib.env.wrappers.recsim_wrapper import make_recsim_env
+
 from slateq_from_scratch.userchoice import UserChoiceModel
+from slateq_from_scratch.qmodel import QModel
 
 
 class ReplayBuffer:
@@ -38,7 +40,12 @@ def main():
     optimizer = torch.optim.Adam(user_choice_model.parameters(), lr=0.01)
     loss_fn = nn.CrossEntropyLoss(reduction="sum")
 
+    q_model = QModel()
+    q_optimizer = torch.optim.Adam(q_model.parameters())
+    q_loss_fn = nn.MSELoss(reduction="sum")
+
     for idx_episode in range(1000):
+        episode_reward = 0.0
         obs = env.reset()
         done = False
         entry = {
@@ -52,17 +59,17 @@ def main():
             "next_action": None,
         }
         last_entry = {}
-        step = 0
+        episode_step = 0
         while not done:
             entry = {}
             entry["state_user"] = pack_state_user(obs)
             entry["state_doc"] = pack_state_doc(obs)
 
-            action = compute_action(obs, user_choice_model)
+            action = compute_action(obs, user_choice_model, q_model)
 
             entry["action"] = np.array(action, dtype=np.int32)
 
-            step += 1
+            episode_step += 1
             obs, reward, done, info = env.step(action)
             if last_entry:
                 last_entry["next_action"] = np.array(action, dtype=np.int32)
@@ -71,6 +78,7 @@ def main():
                 buf.add(last_entry)
 
             click_idx, myopic_reward = parse_response(obs["response"])
+            episode_reward += myopic_reward
 
             # if not done:
             #     assert click_idx >= 0, (step, obs, reward, done, info)
@@ -79,10 +87,79 @@ def main():
             entry["myopic_reward"] = np.array(myopic_reward, dtype=np.float32)
 
             last_entry = entry
+
+        print(episode_reward, episode_step)
+
         if (idx_episode + 1) % 10 == 0:
             train_user_choice_model(
-                user_choice_model, loss_fn, optimizer, buf, batch_size=16, num_iters=10
+                user_choice_model, loss_fn, optimizer, buf, batch_size=4, num_iters=100
             )
+            train_q_model(
+                q_model,
+                user_choice_model,
+                q_loss_fn,
+                q_optimizer,
+                buf,
+                batch_size=4,
+                num_iters=30,
+            )
+
+
+def train_q_model(
+    q_model, user_choice_model, q_loss_fn, q_optimizer, buf, batch_size, num_iters
+):
+    tot_loss = 0
+    tot_items = 0
+    for _ in range(num_iters):
+        batch = buf.sample(batch_size)
+
+        next_selected_doc = torch.cat(
+            [
+                torch.index_select(doc, 0, sel).unsqueeze(0)
+                for doc, sel in zip(
+                    batch["next_state_doc"], batch["next_action"].long()
+                )
+            ],
+            dim=0,
+        )  # shape=[batch_size, slate_size, num_embeddings]
+        next_user = batch["next_state_user"]
+        with torch.no_grad():
+            q_values = q_model(
+                next_user, next_selected_doc
+            )  # shape=[batch_size, slate_size]
+            scores = user_choice_model(
+                next_user, next_selected_doc
+            )  # shape=[batch_size, slate_size+1]
+            scores = torch.exp(scores - torch.max(scores, dim=1, keepdim=True)[0])
+            next_q_values = torch.sum(q_values * scores[:, :-1], dim=1) / torch.sum(
+                scores, dim=1
+            )  # shape=[batch_size]
+        target_q_values = next_q_values + batch["myopic_reward"]  # shape=[batch_size]
+
+        selected_doc = torch.cat(
+            [
+                torch.index_select(doc, 0, sel).unsqueeze(0)
+                for doc, sel in zip(batch["state_doc"], batch["action"].long())
+            ],
+            dim=0,
+        )  # shape=[batch_size, slate_size, num_embeddings]
+        user = batch["state_user"]
+        q_values = q_model(user, selected_doc)  # shape=[batch_size, slate_size]
+        scores = user_choice_model(
+            user, selected_doc
+        )  # shape=[batch_size, slate_size+1]
+        scores = torch.exp(scores - torch.max(scores, dim=1, keepdim=True)[0])
+        q_values = torch.sum(q_values * scores[:, :-1], dim=1) / torch.sum(
+            scores, dim=1
+        )  # shape=[batch_size]
+
+        loss = q_loss_fn(q_values, target_q_values)
+        q_optimizer.zero_grad()
+        loss.backward()
+        q_optimizer.step()
+        tot_loss += loss.item()
+        tot_items += batch_size
+    print(tot_loss / tot_items)
 
 
 def train_user_choice_model(model, loss_fn, optimizer, buf, batch_size, num_iters):
@@ -115,17 +192,15 @@ def pack_state_doc(obs):
     return np.array(list(obs["doc"].values()), dtype=np.float32)
 
 
-def compute_action_greedy(obs, user_choice_model):
-    user = obs["user"]
-    doc = np.stack(list(obs["doc"].values()), axis=0)
-    scores = np.einsum("c,dc->d", user, doc)
-    n_docs = len(scores)
+def compute_action_random(obs, user_choice_model):
+    n_docs = len(obs["doc"])
     selected = np.random.choice(list(range(n_docs)), 3)
     action = tuple(selected)
     return action
 
 
-def compute_action(obs, user_choice_model):
+def compute_action_choice_model(obs, user_choice_model, q_model):
+    """Select docs with highest click probability"""
     user = pack_state_user(obs)
     doc = pack_state_doc(obs)
     user = torch.tensor(user).unsqueeze(0)
@@ -137,6 +212,42 @@ def compute_action(obs, user_choice_model):
     scores_doc, score_no_click = scores[:-2], scores[-1]
     selected = np.argsort(scores_doc)[-3:].tolist()
     # print(scores_doc, selected, score_no_click)
+    action = tuple(selected)
+    return action
+
+
+def compute_action(
+    obs, user_choice_model: UserChoiceModel, q_model: QModel, slate_size: int = 3
+):
+    user = pack_state_user(obs)
+    doc = pack_state_doc(obs)
+    user = torch.tensor(user).unsqueeze(0)
+    doc = torch.tensor(doc).unsqueeze(0)
+
+    with torch.no_grad():
+        scores = user_choice_model(user, doc).squeeze(0)  # shape=[num_docs+1]
+        scores = torch.exp(scores - torch.max(scores, dim=-1)[0])
+        scores_doc = scores[:-1]  # shape=[num_docs]
+        score_no_click = scores[-1]  # shape=[]
+        q_values = q_model(user, doc).squeeze(0)  # shape=[num_docs]
+
+    num_docs = len(obs["doc"])
+    indices = torch.tensor(np.arange(num_docs)).long()
+    slates = torch.combinations(indices, r=slate_size)  # shape=[num_slates, num_docs]
+    num_slates, _ = slates.shape
+
+    slate_decomp_q_values = torch.gather(
+        q_values.unsqueeze(0).expand(num_slates, num_docs), 1, slates
+    )  # shape=[num_slates, slate_size]
+    slate_scores = torch.gather(
+        scores_doc.unsqueeze(0).expand(num_slates, num_docs), 1, slates
+    )  # shape=[num_slates, slate_size]
+    slate_q_values = (slate_decomp_q_values * slate_scores).sum(dim=1) / (
+        slate_scores.sum(dim=1) + score_no_click
+    )  # shape=[num_slates]
+
+    idx = np.argmax(slate_q_values.detach().numpy())
+    selected = slates[idx].detach().numpy().tolist()
     action = tuple(selected)
     return action
 
---
+++ slateq_from_scratch/qmodel.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+
+class QModel(nn.Module):
+    def __init__(self, embedding_size: int = 20):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(embedding_size * 2, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 64),
+            nn.LeakyReLU(),
+            nn.Linear(64, 16),
+            nn.LeakyReLU(),
+            nn.Linear(16, 1),
+        )
+
+    def forward(self, user: torch.Tensor, doc: torch.Tensor) -> torch.Tensor:
+        """Evaluate the user-doc Q model
+
+        Args:
+            user (torch.Tensor): User embedding of shape (batch_size,
+                embedding_size).
+            doc (torch.Tensor): Doc embeddings of shape (batch_size,
+                num_docs, embedding_size).
+
+        Returns:
+            score (torch.Tensor): q_values of shape (batch_size, num_docs).
+        """
+        batch_size, num_docs, embedding_size = doc.shape
+        doc_flat = doc.view((batch_size * num_docs, embedding_size))
+        user_repeated = user.repeat(num_docs, 1)
+        x = torch.cat([user_repeated, doc_flat], dim=1)
+        x = self.layers(x)
+        return x.view((batch_size, num_docs))
+
+
+if __name__ == "__main__":
+    m = QModel()
+    user = torch.randn((5, 20))
+    doc = torch.randn((5, 10, 20))
+    q_values = m(user, doc)
